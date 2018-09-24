9062f4f2c80d4d5161fd5f30c965ded56fcf8327
abdheshkumar <abdhesh@callhandling.co.uk>
2018-09-24 10:53:07 +0100

    Added chmod permission

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-curl -L -o /usr/local/bin/coursier https://git.io/vgvpD
+curl -L -o /usr/local/bin/coursier https://git.io/vgvpD && chmod +x /usr/local/bin/coursier
 coursier bootstrap com.geirsson:scalafmt-cli_2.12:1.5.1 \
   -r bintray:scalameta/maven \
-  -o /usr/local/bin/scalafmt -f --standalone --main org.scalafmt.cli.Cli
+  -o /usr/local/bin/scalafmt -f && chmod +x /usr/local/bin/scalafmt --standalone --main org.scalafmt.cli.Cli
 scalafmt --version
